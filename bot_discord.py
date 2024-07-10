fd4b8e54f363919e5e6b6f92947fa81c1c9e58d4
Yan1ch9 <152898230+Yan1ch9@users.noreply.github.com>
2024-07-10 20:53:36 +0300

    Update bot_discord.py

--- bot_discord.py
+++ bot_discord.py
@@ -1,28 +1,21 @@
 import discord
+from discord.ext import commands
 from bot_logic import generate_password
-
-# Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='/', intents=intents)
+
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$pass'):
-        password = generate_password(10)
-        await message.channel.send(password)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
 
-
-client.run("TOKEN")
+@bot.command(name="password")
+async def password1(ctx, length):
+    password = generate_password(length)
+    await ctx.send(password)
+    await ctx.send(length)
+bot.run("")
